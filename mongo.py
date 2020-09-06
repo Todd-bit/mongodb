a58b0da12c3bdd7b27c2dc4ac9ed1bca493660fd
Todd-bit <todd7385@gmail.com>
2020-09-06 12:53:11 +0000

    Initial commit

---
+++ mongo.py
@@ -0,0 +1,26 @@
+import pymongo
+import os
+if os.path.exists("env.py"):
+    import env
+
+MONGODB_URI = os.environ.get("MONGO_URI")
+DATABASE = "myTestDB"
+COLLECTION = "myFirstMDB"
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+        
+        
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DATABASE][COLLECTION]
+
+documents = coll.find()
+
+for doc in documents:
+    print(doc)
